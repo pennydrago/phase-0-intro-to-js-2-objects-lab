55654811c5b16f8f8b11c0cbb4bb4710ce703643
Penelope Drago <prd@drago.com.au>
2023-03-21 16:06:59 +1100

    add functions attempt 2

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+// Employee object
+const employee = {
+  name: "Employee1",
+  streetAddress: "1 Test Street",
+}
+
+// Function to return a new employee object with an updated value
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = {...employee};
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+// Function to update the employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to return a new employee object with a removed property
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to remove a property from the employee object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
